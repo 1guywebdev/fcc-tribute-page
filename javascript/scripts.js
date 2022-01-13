5b1db8084f5f737a062c6dbdbcc332f7d4305880
1guywebdev <88608510+1guywebdev@users.noreply.github.com>
2022-01-13 13:18:49 +0200

    Update scripts.js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,1 +1,16 @@
+var modal = document.getElementById("modal");
 
+var img = document.getElementById("image");
+var modalImg = document.getElementById("modal-image");
+var imgText = document.getElementById("img-caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  imgText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("close-modal")[0];
+
+span.onclick = function(){
+  modal.style.display = "none";
+}
